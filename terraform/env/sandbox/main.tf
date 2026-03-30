56c9e07a8ec38d657554829a9400815cc2d3157a
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2026-03-30 19:18:31 +0900

    Add environment variable

--- terraform/env/sandbox/main.tf
+++ terraform/env/sandbox/main.tf
@@ -13,6 +13,7 @@ module "weather_forecast_bot" {
     timeout     = 15
     memory_size = 128
     environment_variables = {
+      WEATHER_BOT_RUNTIME           = "lambda"
       WEATHER_API_ACCESS_TOKEN_PATH = "weather-api-access-token"
       MISSKEY_ACCESS_TOKEN_PATH     = "misskey-access-token"
       WEATHER_API_ENDPOINT          = "https://api.weatherapi.com/v1/forecast.json"
