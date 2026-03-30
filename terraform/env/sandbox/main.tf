f8b682ef6d20dc4cc0de9f505b6c66a2ec497040
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2026-03-30 20:50:15 +0900

    Remove environment variable

--- terraform/env/sandbox/main.tf
+++ terraform/env/sandbox/main.tf
@@ -13,7 +13,6 @@ module "weather_forecast_bot" {
     timeout     = 15
     memory_size = 128
     environment_variables = {
-      WEATHER_BOT_RUNTIME           = "lambda"
       EMOJI_CSV_FILEPATH            = "/var/runtime/Data/weather_conditions.csv"
       WEATHER_API_ACCESS_TOKEN_PATH = "weather-api-access-token"
       MISSKEY_ACCESS_TOKEN_PATH     = "misskey-access-token"
