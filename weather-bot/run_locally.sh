a8d384e8140791ea9dce3ce4797a72a19775834b
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2026-03-30 10:13:25 +0900

    Update script

--- weather-bot/run_locally.sh
+++ weather-bot/run_locally.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUST_LOG=info cargo run -- \
+RUST_LOG=info cargo run --no-default-features --features local -- \
     --weather-api-query Tokyo \
     --weather-api-days 1 \
     --misskey-server-url https://misskey-dabansky.com
